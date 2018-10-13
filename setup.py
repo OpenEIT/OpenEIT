0f739ed3d4a6c0d4c23d61732c4c3e3b48bc46b4
Marion Le Borgne <leborgne.mar@gmail.com>
2018-10-13 16:26:45 -0700

    add missing comma in setup.py (at L9) + replace "~=" by "==" in requirements

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ VERSION = '0.1.0'
 AUTHOR = 'Jean Rintoul'
 LICENSE = 'LICENSE'
 INSTALL_REQUIRES = [
-                    'imageio~=2.2.0',
-                    'matplotlib~=2.1.1'
-                    'numpy~=1.14.0',
-                    'pyserial~=3.4',
-                    'scikit-image~=0.13.1',
-                    'scipy~=1.0.0',
-                    'six~=1.11.0',
+                    'imageio==2.2.0',
+                    'matplotlib==2.1.1',
+                    'numpy==1.14.0',
+                    'pyserial==3.4',
+                    'scikit-image==0.13.1',
+                    'scipy==1.0.0',
+                    'six==1.11.0',
                 ]
 
 setup(
