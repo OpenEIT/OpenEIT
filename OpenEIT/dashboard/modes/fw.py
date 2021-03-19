f5cf0ddc96782331ce37cad8c8d08813e2f12913
Sascha Spors <Sascha.Spors@uni-rostock.de>
2021-03-19 15:52:59 +0100

    Compatibility with plotly v4

--- OpenEIT/dashboard/modes/fw.py
+++ OpenEIT/dashboard/modes/fw.py
@@ -10,7 +10,7 @@ import dash
 from dash.dependencies import Output,Input,State
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.plotly as py
+import chart_studio.plotly as py
 import plotly.graph_objs as go
 from flask import send_from_directory
 import serial.tools.list_ports
